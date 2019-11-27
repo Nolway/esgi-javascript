5f3bc0322a0a1fe576a8cb82c24b70d58ffaaeb3
Syanka <faizeau.alexis@gmail.com>
2019-11-27 12:43:17 +0100

    Fix type check v1

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,7 +1,7 @@
 function type_check_v1(v, t) {
   if (typeof t !== "string" || t.length === 0) return false;
   if (t === "null") {
-    return t === null;
+    return v === null;
   } else if (t === "array") {
     return Array.isArray(v);
   }
